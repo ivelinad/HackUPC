722ebed5f263d42d8dcaceccc3bfa0784d9d1a72
Daumantas <daumis05@gmail.com>
2018-10-19 21:22:32 +0100

    added activities

---
+++ app/src/main/java/v2/hackupc/guts2018/ciudadnube/AddDetailsActivity.java
@@ -0,0 +1,13 @@
+package v2.hackupc.guts2018.ciudadnube;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_details);
+    }
+}
